b1c4567efaa5ff953fe7ba93ba00d5ff8809e7c4
Alex Castle <agcastle64@gmail.com>
2016-03-05 15:46:14 -0500

    One more time without spaces... derp

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-CONFIG = "/app/config/mumble-server.ini"
-CERT = "/app/certs/mumble.csr"
-CERT_KEY = "/app/certs/mumble.key"
+CONFIG="/app/config/mumble-server.ini"
+CERT="/app/certs/mumble.csr"
+CERT_KEY="/app/certs/mumble.key"
 
-if [ ! -f "$CONFIG" ]; then
+if [ ! -f $CONFIG ]; then
   echo "Making mumble-server.ini"
-  cp -f /app/config/mumble-server-template.ini "$CONFIG"
-  sed -i -e "s|{{MUMBLE_WELCOME_TEXT}}|${MUMBLE_WELCOME_TEXT}|g" "$CONFIG"
-  sed -i -e "s|{{MUMBLE_PASSWORD}}|${MUMBLE_PASSWORD}|g" "$CONFIG"
-  sed -i -e "s|{{MUMBLE_BANDWIDTH}}|${MUMBLE_BANDWIDTH}|g" "$CONFIG"
-  sed -i -e "s|{{MUMBLE_USERS}}|${MUMBLE_USERS}|g" "$CONFIG"
+  cp -f /app/config/mumble-server-template.ini $CONFIG
+  sed -i -e "s|{{MUMBLE_WELCOME_TEXT}}|${MUMBLE_WELCOME_TEXT}|g" $CONFIG
+  sed -i -e "s|{{MUMBLE_PASSWORD}}|${MUMBLE_PASSWORD}|g" $CONFIG
+  sed -i -e "s|{{MUMBLE_BANDWIDTH}}|${MUMBLE_BANDWIDTH}|g" $CONFIG
+  sed -i -e "s|{{MUMBLE_USERS}}|${MUMBLE_USERS}|g" $CONFIG
 fi
 
 if [[ ! (-f "$CERT" && -f "$CERT_KEY" ) ]]; then
