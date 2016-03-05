9f75b71316d0e1ebb68e5dfec0062d4cd6093bcd
Alex Castle <agcastle64@gmail.com>
2016-03-05 15:42:32 -0500

    Wrap vars for interpolation

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,11 @@ CERT_KEY = "/app/certs/mumble.key"
 
 if [ ! -f "$CONFIG" ]; then
   echo "Making mumble-server.ini"
-  cp -f /app/config/mumble-server-template.ini $CONFIG
-  sed -i -e "s|{{MUMBLE_WELCOME_TEXT}}|${MUMBLE_WELCOME_TEXT}|g" $CONFIG
-  sed -i -e "s|{{MUMBLE_PASSWORD}}|${MUMBLE_PASSWORD}|g" $CONFIG
-  sed -i -e "s|{{MUMBLE_BANDWIDTH}}|${MUMBLE_BANDWIDTH}|g" $CONFIG
-  sed -i -e "s|{{MUMBLE_USERS}}|${MUMBLE_USERS}|g" $CONFIG
+  cp -f /app/config/mumble-server-template.ini "$CONFIG"
+  sed -i -e "s|{{MUMBLE_WELCOME_TEXT}}|${MUMBLE_WELCOME_TEXT}|g" "$CONFIG"
+  sed -i -e "s|{{MUMBLE_PASSWORD}}|${MUMBLE_PASSWORD}|g" "$CONFIG"
+  sed -i -e "s|{{MUMBLE_BANDWIDTH}}|${MUMBLE_BANDWIDTH}|g" "$CONFIG"
+  sed -i -e "s|{{MUMBLE_USERS}}|${MUMBLE_USERS}|g" "$CONFIG"
 fi
 
 if [[ ! (-f "$CERT" && -f "$CERT_KEY" ) ]]; then
