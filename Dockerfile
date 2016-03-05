05d44c05462d4314c6bdbe00a678e9a6cca78f47
Alex Castle <agcastle64@gmail.com>
2016-03-05 15:29:41 -0500

    Add self signed cert generation

--- Dockerfile
+++ Dockerfile
@@ -2,22 +2,24 @@ FROM ubuntu:14.04
 MAINTAINER Alex Castle <agcastle64@gmail.com>
 
 ENV MUMBLE_WELCOME_TEXT="<br />Welcome to this server running <b>Murmur</b>.<br />Enjoy your stay!<br />" \
-  MUMBLE_PORT=64738 \
-  MUMBLE_HOST=0.0.0.0 \
   MUMBLE_PASSWORD="" \
   MUMBLE_BANDWIDTH=72000 \
-  MUMBLE_USERS=100
+  MUMBLE_USERS=100 \
+  HOME = /app
 
-ADD mumble-server.ini /etc/mumble/mumble-server-template.ini
+ADD mumble-server.ini /app/config/mumble-server-template.ini
 ADD entrypoint.sh /app/entrypoint.sh
   
 RUN useradd mumble \
   && apt-get update \
   && apt-get install -y mumble-server \
-  && mkdir -p /opt/mumble/data \
+  && mkdir -p /app/data \
+  && mkdir -p /app/logs \
+  && mkdir -p /app/certs \
   && chown -R mumble /app \
-  && chown mumble /opt/mumble/data
+  && chmod -R 775 /app
 
+VOLUME ["/app/data", "/app/logs", "/app/certs"]
 EXPOSE 64738
 USER mumble
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,21 @@
-cp -f /etc/mumble/mumble-server-template.ini /etc/mumble/mumble-server.ini
-sed -i -e 's/{{MUMBLE_WELCOME_TEXT}}/${MUMBLE_WELCOME_TEXT}/g' /etc/mumble/mumble-server.ini
-sed -i -e 's/{{MUMBLE_PORT}}/${MUMBLE_PORT}/g' /etc/mumble/mumble-server.ini
-sed -i -e 's/{{MUMBLE_HOST}}/${MUMBLE_HOST}/g' /etc/mumble/mumble-server.ini
-sed -i -e 's/{{MUMBLE_PASSWORD}}/${MUMBLE_PASSWORD}/g' /etc/mumble/mumble-server.ini
-sed -i -e 's/{{MUMBLE_BANDWIDTH}}/${MUMBLE_BANDWIDTH}/g' /etc/mumble/mumble-server.ini
-sed -i -e 's/{{MUMBLE_USERS}}/${MUMBLE_USERS}/g' /etc/mumble/mumble-server.ini
+#!/bin/bash
 
-/usr/sbin/murmurd -fg -ini "/etc/mumble/mumble-server.ini"
+CONFIG = "/app/config/mumble-server.ini"
+CERT = "/app/certs/mumble.csr"
+CERT_KEY = "/app/certs/mumble.key"
+
+if [ ! -f "$CONFIG" ]; then
+  echo "Making mumble-server.ini"
+  cp -f /app/config/mumble-server-template.ini $CONFIG
+  sed -i -e "s/{{MUMBLE_WELCOME_TEXT}}/${MUMBLE_WELCOME_TEXT}/g" $CONFIG
+  sed -i -e "s/{{MUMBLE_PASSWORD}}/${MUMBLE_PASSWORD}/g" $CONFIG
+  sed -i -e "s/{{MUMBLE_BANDWIDTH}}/${MUMBLE_BANDWIDTH}/g" $CONFIG
+  sed -i -e "s/{{MUMBLE_USERS}}/${MUMBLE_USERS}/g" $CONFIG
+fi
+
+if [[ ! (-f "$CERT" && -f "$CERT_KEY" ) ]]; then
+  echo "Creating mumble.key and mumble.csr"
+  openssl req -nodes -newkey rsa:4096 -keyout /app/certs/mumble.key -out /app/certs/mumble.csr -subj "/CN=${MUMBLE_HOST}"
+fi
+
+/usr/sbin/murmurd -fg -ini "/app/config/mumble-server.ini"
